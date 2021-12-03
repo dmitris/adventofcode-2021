e9d94c07c0517f4dde808d680d0902ed69429725
Dmitry S <dsavints@gmail.com>
2021-12-03 18:55:56 +0100

    day03 part2

--- day03/src/main.rs
+++ day03/src/main.rs
@@ -7,8 +7,8 @@ fn main() -> Result<()> {
     io::stdin().lock().read_to_string(&mut input)?;
     let result = part1(&input)?;
     writeln!(io::stdout(), "part 1: {}", result)?;
-    // let result = part2(&input)?;
-    // writeln!(io::stdout(), "part 2: {}", result)?;
+    let result = part2(&input)?;
+    writeln!(io::stdout(), "part 2: {}", result)?;
     Ok(())
 }
 
@@ -53,4 +53,86 @@ fn track_to_number(v: &Vec<i16>) -> Result<(usize, usize)> {
         }
     }
     Ok((gamma,epsilon))
+}
+
+use std::collections::HashSet;
+fn part2(input: &str) -> Result<usize> {
+    let mut oxygen = HashSet::new();
+    let mut scrubber = HashSet::new();
+    let mut track: Vec<i16> = vec![];
+    for (i, line) in input.lines().enumerate() {
+        let num = usize::from_str_radix(line, 2)?;
+        oxygen.insert(num);
+        scrubber.insert(num);
+        if i == 0 {
+            for _ in 0..line.len() {
+                track.push(0); // initialise
+            }
+        } else { // validate the same length of each record
+            if line.len() != track.len() {
+                return Err(format!("bad input line {} - must be exactly {} chars (each 0 or 1)", line, track.len()).into());
+            }
+        }
+        let b = line.as_bytes();
+        for j in 0..track.len() {
+            match b[j] {
+                0x30 => track[j] -= 1,
+                0x31 => track[j] += 1,
+                _ => return Err(format!("bad input line {} only '0' and '1' allowed", line).into()),
+            }
+        }                
+    }
+    let (oxy_rating, scrubber_rating) = helper_part2(track.len(), oxygen, scrubber)?;
+    Ok(oxy_rating * scrubber_rating)
+}
+
+fn helper_part2(rec_len: usize, mut oxygen: HashSet<usize>, mut scrubber: HashSet<usize>) -> Result<(usize, usize)> {
+    if oxygen.len() == 0 || scrubber.len() == 0 {
+        return Err("bad input - oxygen and/or scrubber sets are empty".into());
+    }
+    let mut mask = 1 << rec_len-1;
+    for _ in 0..rec_len {
+        if oxygen.len() == 1 && scrubber.len() == 1 {
+            break;
+        }
+        if oxygen.len() > 1 {
+            let (zeros, ones) = count_zeros_ones(&oxygen, mask);
+            if ones >= zeros { 
+                oxygen.retain(|&x| (x & mask) > 0);
+            } else {
+                oxygen.retain(|&x| (x & mask) == 0);
+            }          
+        }
+        if scrubber.len() > 1 {
+            let (zeros, ones) = count_zeros_ones(&scrubber, mask);
+            if zeros <= ones {
+                scrubber.retain(|&x| (x & mask) == 0);
+            } else {
+                scrubber.retain(|&x| (x & mask) > 0);
+            }          
+        }
+        mask = mask >> 1;
+    }
+
+    if oxygen.len() != 1  {
+        return Err(format!("more than one number left unfiltered in oxygen - len: {}", oxygen.len()).into());
+    }
+    if scrubber.len() != 1  {
+        return Err(format!("more than one number left unfiltered in scrubber - len: {}", scrubber.len()).into());
+    }
+    Ok( (oxygen.into_iter().next().unwrap(), scrubber.into_iter().next().unwrap()) )
+}
+
+fn count_zeros_ones(s: &HashSet<usize>, mask: usize) -> (usize, usize) {
+    let mut zeros = 0;
+    let mut ones = 0;
+
+    for e in s {
+        if *e & mask > 0 {
+            ones += 1;
+        } else {
+            zeros += 1;
+        }
+    }
+    (zeros, ones)
 }
